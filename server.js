5eb5d3e4ba3a48d014e7cbe78e8ab9777e658ad2
Abdul Halim <ahr.web.pro@gmail.com>
2023-01-23 15:11:01 +0600

    Update server file for accessing all requests

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const jsonServer = require("json-server");
+const clone = require("clone");
+const data = require("./db.json");
+
+const isProductionEnv = process.env.NODE_ENV === "production";
 
 const server = jsonServer.create();
 
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(isProductionEnv ? clone(data) : "db.json", {
+    _isFake: isProductionEnv,
+});
 
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-}));
+server.use((req, res, next) => {
+    if (req.path !== "/") router.db.setState(clone(data));
+    next();
+});
 
 server.use(router);
 
